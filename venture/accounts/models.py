2dbad737504af70175550abcb36099e8b1997a7e
Jherika Silva <pereirajherika@gmail.com>
2025-12-10 21:20:33 -0300

    feat: customizacao user

--- venture/accounts/models.py
+++ venture/accounts/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+import os
 
-# Create your models here.
+def avatar_upload_to(instance, filename):
+    """
+        Define a lógica e o caminho de armazenamento para os arquivos de imagem do avatar do usuário no sistema, 
+    o objetivo principal é garantir que cada arquivo seja armazenado de forma única, segura e organizada pelo ID do Usuário Individual
+    """
+    ext = os.path.splitext(filename)[1]  
+    
+    filename = f"{uuid.uuid4()}{ext}"
+    
+    user_id = instance.id or "temp"
+    
+    return f'avatars/user_{user_id}/{filename}'
+
+class User(AbstractUser):
+    
+    bio = models.TextField(blank=True)
+    interests = models.CharField(max_length=255, blank=True)
+    avatar = models.ImageField(
+        upload_to=avatar_upload_to,
+        blank=True,
+        null=True,
+        default='avatars/default.png'
+    )
+
+   
+    xp = models.PositiveIntegerField(default=0)
+    level = models.PositiveIntegerField(default=1)
+
+   
+    def xp_for_next_level(self) -> int:
+        """
+        Retorna quanto de XP é necessário para atingir o próximo nível
+        """
+        return 100 * self.level
+
+    def add_xp(self, amount: int):
+        """
+        Adiciona XP."""
+        
+        if amount <= 0:
+            return
+
+        self.xp += amount
+
+        
+        while self.xp >= self.xp_for_next_level():
+            self.xp -= self.xp_for_next_level()
+            self.level += 1
+
+       
+        self.save(update_fields=['xp', 'level'])
+
+   
+    @property
+    def xp_percentage(self):
+        """
+        Retorna a porcentagem de XP da barra atual (0 a 100)
+        """
+        return int((self.xp / self.xp_for_next_level()) * 100)
+
+
+    def __str__(self):
+        return f"{self.username} (Level {self.level})"
--- venture/venturegotchi/settings.py
+++ venture/venturegotchi/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    
 ]
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
