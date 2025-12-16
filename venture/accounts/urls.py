2bbdbf430afe268c4bddf26c1659d19eb84e8ed6
Jherika Silva <pereirajherika@gmail.com>
2025-12-16 16:01:46 -0300

    feat: adicionei urls

--- venture/accounts/urls.py
+++ venture/accounts/urls.py
@@ -1,9 +1,13 @@
 
 from django.urls import path
 from . import views
+from views import HomeView,ProfileView
+
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('', HomeView.as_view(), name='home'),
+    path('perfil/', ProfileView.as_view(), name='profile')
     
 ]
