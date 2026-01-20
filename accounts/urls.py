6b9732727b8bc126cc201992261edaf4f9cfc557
Pericles Junior <184568632+PericlesJunior1212@users.noreply.github.com>
2026-01-19 21:36:41 -0300

    corrigindo bugs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from views import HomeView,ProfileView
+from .views import HomeView,ProfileView
 
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('', HomeView.as_view(), name='home'),
-    path('perfil/', ProfileView.as_view(), name='profile'),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
