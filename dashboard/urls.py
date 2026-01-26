a6df9918d46727de489dce248b80e017c273b50f
JherikaSilva <pereirajherika@gmail.com>
2026-01-25 23:07:01 -0300

    feat: adicionei url mentor, empresa e ranking

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import dashboard_home
+from .views import dashboard_home,mentor_panel, company_panel,leaderboard
 
 urlpatterns = [
     path("", dashboard_home, name="dashboard"),
+    path("mentor/", mentor_panel, name="mentor_panel"),
+    path("empresa/", company_panel, name="company_panel"),
+    path("ranking/", leaderboard, name="leaderboard"),
 ]
 
 
