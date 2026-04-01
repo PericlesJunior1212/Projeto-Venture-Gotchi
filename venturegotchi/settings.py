5126ae129cb03544b716807d34787246680c2b12
Pericles Junior <periclesjunior1981@gmail.com>
2026-03-31 22:43:48 -0300

    up

--- venturegotchi/settings.py
+++ venturegotchi/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY', '12345678')
+SECRET_KEY = os.environ.get('SECRET_KEY', '12345678')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
