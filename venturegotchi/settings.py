157bf955fb81a7f9854c8c36680d45cbdbfab63a
Pericles Junior <periclesjunior1981@gmail.com>
2026-04-02 19:47:56 -0300

    up

--- venturegotchi/settings.py
+++ venturegotchi/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-build-fallback-key-xy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('.railway.app,projeto-venture-gotchi-production.up.railway.app', '127.0.0.1,localhost').split(',')
 
 # Application definition
 INSTALLED_APPS = [
